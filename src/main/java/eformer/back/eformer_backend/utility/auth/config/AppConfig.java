70717e49e59bfae8b7107a150f3fcf8771c50b22
Yaman <yamanserajbs@gmail.com>
2023-05-18 01:09:15 +0300

    Need Orders API. Need Stats API. Working on authentication.

--- src/main/java/eformer/back/eformer_backend/utility/auth/config/AppConfig.java
+++ src/main/java/eformer/back/eformer_backend/utility/auth/config/AppConfig.java
@@ -3,8 +3,10 @@ package eformer.back.eformer_backend.utility.auth.config;
 import eformer.back.eformer_backend.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +25,11 @@ public class AppConfig {
         return username -> usersManager.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException(username + " not found")
         );
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
