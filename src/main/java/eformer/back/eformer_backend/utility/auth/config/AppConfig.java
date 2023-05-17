03c3bf0d9bbae90e3be09f5b84689abe7dce1fb5
Yaman <yamanserajbs@gmail.com>
2023-05-18 01:06:59 +0300

    Need Orders API. Need Stats API. Working on authentication.

--- src/main/java/eformer/back/eformer_backend/utility/auth/config/AppConfig.java
+++ src/main/java/eformer/back/eformer_backend/utility/auth/config/AppConfig.java
@@ -1,10 +1,14 @@
-package eformer.back.eformer_backend.utility.auth;
+package eformer.back.eformer_backend.utility.auth.config;
 
 import eformer.back.eformer_backend.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -19,5 +23,20 @@ public class AppConfig {
         return username -> usersManager.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException(username + " not found")
         );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
     }
 }
---
+++ src/main/java/eformer/back/eformer_backend/utility/auth/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package eformer.back.eformer_backend.utility.auth.config;
+
+
+import eformer.back.eformer_backend.utility.auth.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("") // Permit these requests
+                .permitAll()
+                .anyRequest() // These require authentication
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Require token for each request
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
